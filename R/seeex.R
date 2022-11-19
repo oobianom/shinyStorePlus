4dd9440d393801c0d6e161842d0958eb14d39771
Obi Obianom <oobianom@umaryland.edu>
2022-11-19 02:50:19 -0500

    example selections

--- R/seeex.R
+++ R/seeex.R
@@ -2,6 +2,8 @@
 #'
 #' Example of a shiny application with secure in-browser storage of inputs when changed
 #'
+#' @param name the name of example to view. choices include storeInputs or browserLinkToInput
+#'
 #' @note Changes made to the input will be saved and returned when the page is refresh within the same browser over different sessions
 #' @return An example of inputs persistently stored when changed and the page refreshed
 #' @examples
@@ -13,9 +15,10 @@
 #'
 #' @export
 
-seeexample <- function() {
-  ssp <- "shinyStorePlus"
-  ex.dir <- file.path(find.package(package = ssp), "example")
+seeexample <- function(name = c("storeInputs","browserLinkToInput")) {
+  name <- match.arg(name) #get the example to view
+  ssp <- "shinyStorePlus" #set the name of the package
+  ex.dir <- file.path(find.package(package = ssp), "example",name) #path to the example
   message(paste0("The source files for this example is located at ", ex.dir))
   if (interactive()) {
     shiny::runApp(ex.dir)
