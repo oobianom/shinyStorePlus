249e2b350b04e337e1684ab9b34efbf92a70fce6
Obi Obianom <oobianom@umaryland.edu>
2023-11-22 06:52:58 -0500

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -7,3 +7,10 @@
 #'
 
 utils::globalVariables(c("updateSelectInput", "updateCheckboxGroupInput", "updateDateInput", "updateDateRangeInput", "updateCheckboxInput", "updateRadioButtons", "updateTextInput", "renderText"))
+
+
+
+# Given a vector or list, drop all the NULL items in it
+dropNulls <- function(x) {
+  x[!vapply(x, is.null, FUN.VALUE=logical(1))]
+}
