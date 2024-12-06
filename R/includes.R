d982f97da1bbe93fe2bf70b641036f7f7426dfad
Obi Obianom <oobianom@umaryland.edu>
2024-12-05 21:02:31 -0500

    Update includes.R

--- R/includes.R
+++ R/includes.R
@@ -68,6 +68,44 @@
 #'   }
 #'   shiny::shinyApp(ui = ui, server = server)
 #' }
+#'
+#'
+#' # Example app that stores App user views, likes and followers
+#'
+#' library(shiny)
+#' library(shinyStorePlus)
+#'
+#' if (interactive()) {
+#' ui <- fluidPage(
+#'   titlePanel("Simplified shiny app storage of views, likes and followers"),
+#'   initStore("all",rpkg.api.key =
+#'   "c20c5eead7714c119dd3f20bd249a388e72db2aa0f9305d0380b683a37c5296a"),
+#'   h2("Save App Views"),hr(),
+#'   viewsBox("viewsshow","loading views..."),
+#'   h2("Save App Likes, and allow user to Like!"),hr(),
+#'   lfButton("liket",suffix="likes"),
+#'   h2("Save App Followers, and allow user to Follow!"),hr(),
+#'   lfButton("followt",suffix="followers"),
+#'   hr(),p(p(i("Like or Follow and Refresh the page -
+#'   the values are saved and the views are incremented.")))
+#' )
+#'
+#' server <- function(input, output, session) {
+#'   # set up views, likes and follows, leave as
+#'   # NULL if you don't need tracking for either
+#'   # in this case, we leave followID as NULL
+#'   # since we don't need to use that
+#'   setupRPKG(
+#'     viewsID = "viewsshow",
+#'     likesID = "liket",
+#'     followID = "followt"
+#'  )
+#' }
+#'
+#'
+#' shinyApp(ui = ui, server = server)
+#' }
+#'
 #' }
 #'
 #' @export
@@ -85,6 +123,8 @@ initStore <- function(src = c("browser","rpkg","all"), rpkg.api.key) {
   )
 }
 
+
+# sub functions for inclusion of scripts
 
 
 initBRStore <- function() {
