026d2b13ca1747d59fd7bf0121b0bcb84332595b
Obi Obianom <oobianom@umaryland.edu>
2025-01-14 04:01:32 -0500

    Update app.R

--- inst/example/shinyObserveOnce/app.R
+++ inst/example/shinyObserveOnce/app.R
@@ -2,8 +2,11 @@ library(shiny)
 library(shinyStorePlus)
 ui <- fluidPage(
   titlePanel("A shiny app that shows welcome message only once accross multple sessions. Refresh and see it in action"),
-  initStore("browser"),
-  shiny::mainPanel(
+  initStore("browser"),sidebarPanel(
+    sliderInput("slidex", label = "Sample slider", min = 1, max = 50, value = 30)
+
+  ),
+  shiny::mainPanel(width=6,textOutput("slidexresres"),
          "The current function in the 'Shiny' app
          enables users to execute an expression once
          across multiple sessions, ensuring that
@@ -23,21 +26,19 @@ ui <- fluidPage(
 server <- function(input, output, session) {
   # setup for in browser tracking
   observeOnce({
-
+    output$slidexresres <- renderText(input$slidex)
     showModal(modalDialog(
       title = "Welcome to my app",
       "Welcome to the app! We're excited to have you here.
       Please note that this message will only appear the first time you visit.
       After this, it won’t show up again, even if you refresh or reopen the app.
-      Feel free to explore, and we hope you enjoy your experience!", size = "l"
+      Feel free to explore, and we hope you enjoy your experience!",
+      size = "m"
     ))
-
-
-
   }, input = input, output = output)
 
   # optional: clear the storage for the previous time it was executed
-  #observeOnceRestart()
+  observeOnceRestart()
 }
 
 
